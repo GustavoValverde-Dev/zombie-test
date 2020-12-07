e2c9c84b9e6c914e6e5e3eb490be6f4d62579142
GustavoValverde-Dev <gustavovalverde.dev@gmail.com>
2020-12-06 21:33:33 -0300

    front-end reestruturado para adaptação

--- front-end/reduxapp/src/components/redux/resource/rootReducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux'  
-import resourcesReducer from './resourcesReducer'  
-  
-const rootReducer = combineReducers({  
-    resources: resourcesReducer  
-})  
-  
-export default rootReducer  
